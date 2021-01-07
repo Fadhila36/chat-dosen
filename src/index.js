63058bf0c2633ad9dafd67df76a8495aed9efd2e
Fadhila36 <yuffa36@gmail.com>
2021-01-07 16:22:21 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { SnackbarProvider } from 'notistack';
+import { Button, Typography } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const notistackRef = React.createRef();
+const onClickDismiss = key => () => {
+    notistackRef.current.closeSnackbar(key);
+}
+
+
+ReactDOM.render(
+    <SnackbarProvider
+        maxSnack={1}
+        ref={notistackRef}
+        action={(key) => (
+            <Button onClick={onClickDismiss(key)} style={{ color: 'white', fontWeight: 'bold' }}>
+                Dismiss
+            </Button>
+        )}
+    >
+
+        <AppBar position="static" style={{ padding: 0 }}>
+            <Toolbar>
+                <Typography variant="h4">
+                    Web Chat Dosen Beta 1 Himatif UBP KARAWANG
+                    </Typography>
+            </Toolbar>
+        </AppBar>
+        <App />
+
+
+    </SnackbarProvider>
+    ,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
